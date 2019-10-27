2b4ec3aa47a3d73742d7774aa2e9687e3ade696e
Marco Metz <marco.metz@yahoo.de>
2019-10-27 09:46:09 -0500

    Added JS code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,7 +1,18 @@
-$(".open").on("click", function () {
-    $(".popup, .popup-content").addClass("active");
-});
+var carType = document.getElementById("carType");
+var carUse = document.getElementById("carUse");
+var carOwner = document.getElementById("carOwner");
+var value = ["sedan", "van", "truck", "sport", "luxury"];
 
-$(".close, .popup").on("click", function () {
-    $(".popup, .popup-content").removeClass("active");
-});
+function InfoUser(e) {
+    var answerUser = "";
+    switch (e) {
+        answerUser =
+    }
+
+
+    $("input[type='submit']").onclick(function(event) {
+        // user hit the enter key in the input
+        // grab input 
+        var carType = $(this).val();
+        $(this).val(""); //clearing out input field    
+    });
