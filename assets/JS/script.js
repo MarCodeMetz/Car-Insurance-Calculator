f8bb2f63ac1953cb5140a64f2db9baaf1f6910fb
Marco Metz <marco.metz@yahoo.de>
2019-12-29 13:20:21 -0600

    changes in js code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,17 @@
-var carType = document.getElementById("carType");
-var carUse = document.getElementById("carUse");
-var carOwner = document.getElementById("carOwner");
-var liability = document.getElementById("liability");
-var comp = document.getElementById("comp");
-var coll = document.getElementById("coll");
+function insurancePremium() {
+
+    var carType = document.getElementById("carType").value;
+    var carUse = document.getElementById("carUse").value;
+    var carOwner = document.getElementById("carOwner").value;
+    var liability = document.getElementById("liability").value;
+    var comp = document.getElementById("comp").value;
+    var coll = document.getElementById("coll").value;
+    var valuesForPremium = Number(carType.value) + Number(carUse.value) + Number(carOwner.value) + Number(liability.value) + Number(comp.value) + Number(coll.value);
+}
+console.log(valuesForPremium);
 
 
-var insurancePremium() {
-    Number(carType.value + carUse.value + carOwner.value + liability.value + comp.value + coll.value)
-};
-
-console.log(insurancePremium);
+//pop up result, when calculate btn pressed
+function calculateResult() {
+    document.getElementById("quoteResult").innerHTML = "Here is your Quote! $" + valuesForPremium;
+}
